99ad04da0890e93d9804f89df4b445dfab1f217f
llDeRuNll <eduardprudiys@gmail.com>
2025-03-18 06:44:08 -0400

    all

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,44 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.example.com';
-axios.defaults.params = {
-  color: 'black',
-  orientation: 'landscape',
-  per_page: 9,
-};
-export const fetchPhotos = query => {
-  return axios
-    .get(
-      `https://pixabay.com/api/?key=49397541-1939e7fe1ba522fc58da934d1&q=${query}&image_type=photo`
-    )
-    .then(response => response.data.hits);
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '49397541-1939e7fe1ba522fc58da934d1';
+export const fetchPhotos = async query => {
+  try {
+    const response = await axios.get('', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        color: 'black',
+        per_page: 9,
+      },
+    });
+
+    return response.data.hits.map(
+      ({
+        id,
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => ({
+        id,
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      })
+    );
+  } catch (error) {
+    console.error('Error fetching photos:', error);
+    return [];
+  }
 };
